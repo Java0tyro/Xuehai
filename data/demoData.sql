d416206075ddba3780eb90dd3a871c2a3dbc4d81
lirui <lirui494391537@outlook.com>
2018-03-18 17:45:24 +0800

    commit

--- data/demoData.sql
+++ data/demoData.sql
@@ -6,6 +6,8 @@ INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin2", "312433
 INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin3", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin3@admin");
 INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin4", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin4@admin");
 INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin5", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin5@admin");
+INSERT INTO user ( username, `password`,  salt, email, authority) VALUES ("admin", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin@admin", 1);
+
 
 INSERT INTO question_type ( `name` ) VALUES ( "type1" );
 INSERT INTO question_type ( `name`, parent ) VALUES ( "type2" , 1);
