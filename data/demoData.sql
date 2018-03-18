9d2838b10bc8ba88f3f2d63a101eb56b4ef0187b
lirui <lirui494391537@outlook.com>
2018-03-18 15:44:12 +0800

    新增demoData脚本，插入5个测试用户，密码1234

---
+++ data/demoData.sql
@@ -0,0 +1,8 @@
+
+-- 密码 1234
+
+INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin1", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin1@admin");
+INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin2", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin2@admin");
+INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin3", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin3@admin");
+INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin4", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin4@admin");
+INSERT INTO user ( username, `password`,  salt, email) VALUES ("admin5", "312433c28349f63c4f387953ff337046e794bea0f9b9ebfcb08e90046ded9c76", "asdf", "admin5@admin");
